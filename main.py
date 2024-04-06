ebba471a8d1ebe6901c68fe49482a94b33d573f6
Jon Nordby <jononor@gmail.com>
2024-04-06 16:41:25 +0200

    Use FIFO for BMA423 accelerometer
    Requires custom bma423-pure-mp

--- main.py
+++ main.py
@@ -77,6 +77,30 @@ def update_display(display, text):
     print("Ticks per screen fill:", elapsed)
 
 
+def decode_acceleration(sensor, inp, out):
+    # inp is the raw bytes for a set of acceleration samples, typically read from FIFO
+    # NOTE: must be without FIFO headers!
+    
+    bytes_per_sample = 2
+    axes = 3
+    n_samples = len(inp) // (bytes_per_sample*axes)
+    for sample_no in range(0, n_samples):
+        sample_start = sample_no*bytes_per_sample*axes
+        rawdata = memoryview(inp)[sample_start:]
+        acc_x = (rawdata[0] >> 4) | (rawdata[1] << 4)
+        acc_y = (rawdata[2] >> 4) | (rawdata[3] << 4)
+        acc_z = (rawdata[4] >> 4) | (rawdata[5] << 4)
+        acc_x = sensor.convert_to_int12(acc_x)
+        acc_y = sensor.convert_to_int12(acc_y)
+        acc_z = sensor.convert_to_int12(acc_z)
+        acc_x = sensor.normalize_reading(acc_x)
+        acc_y = sensor.normalize_reading(acc_y)
+        acc_z = sensor.normalize_reading(acc_z)
+        out[(sample_no*axes)+0] = acc_x
+        out[(sample_no*axes)+1] = acc_y
+        out[(sample_no*axes)+2] = acc_z
+    
+
 def main():
 
     test_trees() # XXX: fails if done after setup!?
@@ -92,19 +116,43 @@ def main():
     i2c = SoftI2C(sda=10,scl=11)
     sensor = bma423.BMA423(i2c, addr=0x19)
 
+    sensor.set_accelerometer_freq(50)
+    sensor.fifo_enable()
+    sensor.fifo_clear()
+
+    accel_samples = 25
+    accel_buffer = bytearray(accel_samples*3*2) # 3 axes, 2 bytes each
+    accel_array = array.array('f', list(range(0, accel_samples*3)))
 
     while True:
 
         text = str(int(time.ticks_ms()/1000))
-        acc = sensor.get_xyz()
+        #acc = sensor.get_xyz()
         t = 0
         t = sensor.get_temperature()
-        print('acc XYZ', acc, t)
+        #print('acc XYZ', acc, t)
+        #print('temp', t)
         
-        print('set text', text)
-        update_display(display, text)
+        fifo_level = sensor.fifo_level()
+        #print('FIFO level', time.ticks_ms(), fifo_level)
+        if fifo_level >= len(accel_buffer):
+            read_start = time.ticks_ms()
+            sensor.fifo_read(accel_buffer)
+            read_dur = time.ticks_diff(time.ticks_ms(), read_start)
+            
+            decode_start = time.ticks_ms()
+            decode_acceleration(sensor, accel_buffer, accel_array)
+            decode_dur = time.ticks_diff(time.ticks_ms(), read_start)
+            x,y,z = accel_array[0:3]
+
+            print('fifo-read', read_start/1000, fifo_level, read_dur, decode_dur,
+                  '{0:.2f},{1:.2f},{2:.2f}'.format(x, y, z))
+            
+
+        #print('set text', text)
+        #update_display(display, text)
     
-        # Pause 2 seconds.
-        time.sleep(2)
+        # Wait
+        time.sleep(0.10)
 
 main()
